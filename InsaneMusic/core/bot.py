4db5ae87d06cbc1f01e4e57409d6bd249cd0a6f3
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-15 19:22:59 +0530

    Update bot.py

--- InsaneMusic/core/bot.py
+++ InsaneMusic/core/bot.py
@@ -35,9 +35,9 @@ class InsaneBot(Client):
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
-            sys.exit()
+        #if a.status != "administrator":
+            #LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            #sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
